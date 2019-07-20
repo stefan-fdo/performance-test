6a53d310cc2b7cffeeec466d9762da9c1876dca5
stefan-fdo <stefan86.cloud@gmail.com>
2019-07-20 18:34:42 +1000

    Updated for secret.

--- performance-test-script/src/main/groovy/io/stefan/performance_test/TestScript2.groovy
+++ performance-test-script/src/main/groovy/io/stefan/performance_test/TestScript2.groovy
@@ -5,8 +5,8 @@ import static io.stefan.performance_test.EnvironmentVariable.DB_SCHMEA
 
 println 'Hello World! - Test Script 2'
 
-def dbSchema = System.getenv().getOrDefault(DB_SCHMEA)
-def dbPassword = System.getenv().getOrDefault(DB_PASSWORD)
+def dbSchema = System.getenv().get(DB_SCHMEA)
+def dbPassword = System.getenv().get(DB_PASSWORD)
 
 println "DB Schmea - ${dbSchema}"
 
