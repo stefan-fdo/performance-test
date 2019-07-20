c45d351a35857d882287bdf6d7b2bc500bb444e2
stefan-fdo <stefan86.cloud@gmail.com>
2019-07-20 13:07:59 +1000

    Added environment variable

---
+++ performance-test-script/src/main/groovy/io/stefan/performance_test/EnvironmentVariable.groovy
@@ -0,0 +1,6 @@
+package io.stefan.performance_test
+
+class EnvironmentVariable {
+
+	static final String DB_CONNECTION_URL = 'db-url'
+}
--- performance-test-script/src/main/groovy/io/stefan/performance_test/TestScript.groovy
+++ performance-test-script/src/main/groovy/io/stefan/performance_test/TestScript.groovy
@@ -1,4 +1,10 @@
 package io.stefan.performance_test
 
+import static EnvironmentVariable.DB_CONNECTION_URL
+
 println 'Hello World!'
+
+def dbConnectionUrl = System.getenv().getOrDefault(DB_CONNECTION_URL, 'default-db-url')
+
+println "DB Connection Url - ${dbConnectionUrl}"
 
